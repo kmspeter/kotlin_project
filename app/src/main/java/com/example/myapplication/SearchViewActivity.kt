5e40b72276a2bab5a879c4653073a19052d632b5
kmspeter <kmspeter@naver.com>
2023-11-09 10:11:51 +0900

    검색에 리스트추가

--- app/src/main/java/com/example/myapplication/SearchViewActivity.kt
+++ app/src/main/java/com/example/myapplication/SearchViewActivity.kt
@@ -7,18 +7,27 @@ import android.os.Bundle
 import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.ActivityScheduleListBinding
 import com.example.myapplication.databinding.ActivitySearchViewBinding
 
 class SearchViewActivity : AppCompatActivity() {
-    val binding by lazy { ActivitySearchViewBinding.inflate(layoutInflater) }
+    lateinit var binding: ActivityScheduleListBinding
 
+    val schedule_datas = arrayOf(
+        Schedule_data("도르트문트vs뉴캐슬","9월 29일"),
+        Schedule_data("샤흐타르vs바르셀로나","10월 3일"),
+        Schedule_data("라치오vs페예노르트","10월 9일"),
+        Schedule_data("AC밀란vs파리 생제르맹","12월 25일")
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityScheduleListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        binding.recschedule.layoutManager = LinearLayoutManager(this)
+        binding.recschedule.adapter = schedule_list_adapter(schedule_datas)
 
         val main = Intent(this, MainActivity::class.java)
-        binding.apiBack.setOnClickListener{startActivity(main)}
+        binding.apiBack.setOnClickListener { startActivity(main) }
     }
 }
