5d523683d093853e695506e053823d7c0734d23f
kmspeter <kmspeter@naver.com>
2023-11-16 01:35:27 +0900

    파이어베이스 리얼타임 데이터베이스 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-database-ktx")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,22 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.firebase.database.FirebaseDatabase
+
 
 class MainActivity : AppCompatActivity() {
 
-    val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+    private val database = FirebaseDatabase.getInstance()
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         val api = Intent(this, ApiActivity::class.java)
-        binding.apiButton.setOnClickListener{startActivity(api)}
+        binding.apiButton.setOnClickListener { startActivity(api) }
 
         val search_view = Intent(this, SearchViewActivity::class.java)
-        binding.searchButton.setOnClickListener{startActivity(search_view)}
+        binding.searchButton.setOnClickListener { startActivity(search_view) }
 
         val schedule = Intent(this, ScheduleListActivity::class.java)
-        binding.addButton.setOnClickListener{startActivity(schedule)}
+        binding.addButton.setOnClickListener { startActivity(schedule) }
     }
 }
