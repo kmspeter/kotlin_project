4e7d1aa221ec87e35f9a36b2138a4587858a785d
kmspeter <kmspeter@naver.com>
2023-12-06 04:44:20 +0900

    리사이클뷰 추가

--- app/src/main/java/com/example/myapplication/ApiActivity.kt
+++ app/src/main/java/com/example/myapplication/ApiActivity.kt
@@ -3,6 +3,9 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +13,9 @@ import com.example.myapplication.databinding.ActivityApiBinding
 import com.google.firebase.database.*
 import java.text.SimpleDateFormat
 import java.util.Locale
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.databinding.TeamScheduleItemBinding
 
 data class MatchSchedule(
     val date: String = "",
@@ -31,6 +37,9 @@ class ApiActivity : AppCompatActivity() {
     private val matchesRef: DatabaseReference = database.getReference("matches")
     private val schedulesRef: DatabaseReference = database.getReference("schedules")
 
+    private val scheduleList = mutableListOf<MatchSchedule>()
+    private val adapter = MatchScheduleAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -51,6 +60,9 @@ class ApiActivity : AppCompatActivity() {
                 Toast.makeText(this, "팀을 선택하세요.", Toast.LENGTH_SHORT).show()
             }
         }
+
+        binding.teamRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.teamRecyclerView.adapter = adapter // adapter 설정
     }
 
     private fun showTeamSelectionDialog() {
@@ -140,12 +152,11 @@ class ApiActivity : AppCompatActivity() {
                 scheduleMap[dateWithZeroPadding] = scheduleItem
             }
 
-            // matches TextView에 텍스트 설정
-            binding.matches.text = scheduleMap.values.joinToString("\n") { it["content"].toString() }
-
             // "확인" 버튼을 눌렀을 때만 Firebase에 변환된 scheduleMap 저장
             binding.apiLoad.setOnClickListener {
-                schedulesRef.setValue(scheduleMap)
+                val updates = mutableMapOf<String, Any>()
+                updates.putAll(scheduleMap) // 새로운 데이터를 추가
+                schedulesRef.updateChildren(updates)
                     .addOnSuccessListener {
                         // 성공 시 처리
                         Toast.makeText(this, "일정이 추가되었습니다.", Toast.LENGTH_SHORT).show()
@@ -156,6 +167,10 @@ class ApiActivity : AppCompatActivity() {
                         Log.e("SaveSchedule", "일정 저장 오류: $it")
                     }
             }
+
+            scheduleList.clear()
+            scheduleList.addAll(matches)
+            adapter.notifyDataSetChanged()
         }
     }
 
@@ -184,6 +199,7 @@ class ApiActivity : AppCompatActivity() {
         matchesRef.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 val allMatches = mutableListOf<MatchSchedule>()
+                scheduleList.clear()
 
                 for (roundSnapshot in dataSnapshot.children) {
                     for (matchSnapshot in roundSnapshot.children) {
@@ -193,26 +209,33 @@ class ApiActivity : AppCompatActivity() {
                         }
                     }
                 }
-
-                // 로그로 확인
-                Log.d("MatchSchedule", "모든 매치: $allMatches")
-
-                // 선택한 팀과 관련된 일정을 표시
-                val team1Matches = allMatches.filter { it.team1 == selectedTeam }
-                val team2Matches = allMatches.filter { it.team2 == selectedTeam }
-
-                // 로그로 확인
-                Log.d("TeamSchedule", "팀 1 매치: $team1Matches")
-                Log.d("TeamSchedule", "팀 2 매치: $team2Matches")
-
-                // saveSelectedTeamSchedule 함수 호출
-                saveSelectedTeamSchedule(team1Matches + team2Matches)
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
-                // 오류 처리
-                Log.e("TeamSchedule", "데이터 검색 오류: ${databaseError.message}")
+                Log.e("MatchSchedule", "Error fetching matches: ${databaseError.message}")
             }
         })
+    }
+
+    inner class MatchScheduleAdapter : RecyclerView.Adapter<MatchScheduleAdapter.ViewHolder>() {
+
+        inner class ViewHolder(private val binding: TeamScheduleItemBinding) : RecyclerView.ViewHolder(binding.root) {
+            fun bind(matchSchedule: MatchSchedule) {
+                binding.matchInfo.text = "${matchSchedule.team1} vs ${matchSchedule.team2} (${formatDate(matchSchedule.date, matchSchedule.time)})"
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = TeamScheduleItemBinding.inflate(inflater, parent, false)
+            return ViewHolder(binding)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val matchSchedule = scheduleList[position]
+            holder.bind(matchSchedule)
+        }
+
+        override fun getItemCount(): Int = scheduleList.size
     }
 }
